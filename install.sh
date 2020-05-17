1349169f60c650bb814767062aa42456b3dd0560
Gyula Kiri <38438001+duszmox@users.noreply.github.com>
2020-05-17 10:29:26 +0200

    auto yes

--- install.sh
+++ install.sh
@@ -1,18 +1,18 @@
 sudo apt-get update
 # sudo apt-get upgrade
 sudo apt install ffmpeg
-sudo apt-get install mencoder
+sudo apt-get -y install mencoder
 sudo sh -c 'echo "deb https://mkvtoolnix.download/ubuntu/ $(lsb_release -sc) main"
 /etc/apt/sources.list.d/bunkus.org.list'
-sudo apt-get install wget
+sudo apt-get -y install wget
 wget -q -O - https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install mkvtoolnix mkvtoolnix-gui
+sudo apt-get -y install mkvtoolnix mkvtoolnix-gui
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
 sudo chmod a+rx /usr/local/bin/youtube-dl
-sudo apt-get install python-pip
+sudo apt-get -y install python-pip
 sudo pip install --upgrade google-api-python-client oauth2client progressbar2
-sudo apt-get install unzip
+sudo apt-get -y install unzip
 sudo cd /usr/bin/
 sudo wget https://github.com/tokland/youtube-upload/archive/master.zip
 sudo unzip master.zip
