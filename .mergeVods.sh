90fdc8ace80e7e4bdf9d47aaddd89f4bfc98a73e
Gyula Kiri <38438001+duszmox@users.noreply.github.com>
2020-05-17 10:37:26 +0200

    fix frame problem

--- .mergeVods.sh
+++ .mergeVods.sh
@@ -6,6 +6,7 @@ echo "2. VOD url-je:"
 read vod2_url
 echo "Hányadik másodperctől kezdődjön az első VOD? [HH:MM:SS] (0 ha ne vágjon az elejéből)"
 read cut1_from_start
+printf "\n\n"
 echo "Hányadik másodperctől érjen a véget az első VOD? (Ha volt vágás az elejéből azt ki kell vonni ebből az időből) [HH:MM:SS] (0 ha ne vágjon az elejéből)"
 read cut1_from_end
 printf "\n\n"
@@ -24,29 +25,30 @@ youtube-dl -f best -o "download1.mp4" $vod1_url
 youtube-dl -f best -o "download2.mp4" $vod2_url
 if [ "$cut1_from_start" = "0" ]; then
     if [ "$cut1_from_end" = "0" ]; then
-        mencoder -oac pcm -ovc copy download1.mp4 -o merge1.mp4
+        mencoder -oac pcm -ovc copy download1.mp4 -o merge1.mp4 -mc 0 -noskip
     else
-        mencoder -endpos $cut1_from_end -oac pcm -ovc copy download1.mp4 -o merge1.mp4
+        mencoder -endpos $cut1_from_end -oac pcm -ovc copy download1.mp4 -o merge1.mp4 -mc 0 -noskip
     fi
 else
     if [ "$cut1_from_end" = "0" ]; then
-        mencoder -ss $cut1_from_start -oac pcm -ovc copy download1.mp4 -o merge1.mp4
+        mencoder -ss $cut1_from_start -oac pcm -ovc copy download1.mp4 -o merge1.mp4 -mc 0 -noskip
     else
-        mencoder -ss $cut1_from_start -endpos $cut1_from_end -oac pcm -ovc copy download1.mp4 -o merge1.mp4
+        mencoder -ss $cut1_from_start -endpos $cut1_from_end -oac pcm -ovc copy download1.mp4 -o merge1.mp4 -mc 0 -noskip
     fi
 fi
 if [ "$cut2_from_start" = "0" ]; then
     if [ "$cut2_from_end" = "0" ]; then
-        mencoder -oac pcm -ovc copy download2.mp4 -o merge2.mp4
+        mencoder -oac pcm -ovc copy download2.mp4 -o merge2.mp4 -mc 0 -noskip
     else
-        mencoder -endpos $cut2_from_end -oac pcm -ovc copy download2.mp4 -o merge2.mp4
+        mencoder -endpos $cut2_from_end -oac pcm -ovc copy download2.mp4 -o merge2.mp4 -mc 0 -noskip
     fi
-fi
+
 else
-if [ "$cut2_from_end" = "0" ]; then
-    mencoder -ss $cut2_from_start -oac pcm -ovc copy download2.mp4 -o merge2.mp4
-else
-    mencoder -ss $cut2_from_start -endpos $cut2_from_end -oac pcm -ovc copy download2.mp4 -o merge2.mp4
+    if [ "$cut2_from_end" = "0" ]; then
+        mencoder -ss $cut2_from_start -oac pcm -ovc copy download2.mp4 -o merge2.mp4 -mc 0 -noskip
+    else
+        mencoder -ss $cut2_from_start -endpos $cut2_from_end -oac pcm -ovc copy download2.mp4 -o merge2.mp4 -mc 0 -noskip
+    fi
 fi
 mkvmerge -o output.mp4 merge1.mp4 \+ merge2.mp4
 youtube-upload --title="$title" --privacy="$privacy" output.mp4
--- .vod.sh
+++ .vod.sh
@@ -19,18 +19,18 @@ youtube-dl -f best -o "download.mp4" $vod_url
 
 if [ "$cut_from_start" = "0" ]; then
     if [ "$cut_from_end" = "0" ]; then
-        mencoder -oac pcm -ovc copy download.mp4 -o output.mp4
+        mencoder -oac pcm -ovc copy download.mp4 -o output.mp4 -mc 0 -noskip
     else
-        mencoder -endpos $cut_from_end -oac pcm -ovc copy download.mp4 -o output.mp4
+        mencoder -endpos $cut_from_end -oac pcm -ovc copy download.mp4 -o output.mp4 -mc 0 -noskip
     fi
 else
     if [ "$cut_from_end" = "0" ]; then
-        mencoder -ss $cut_from_start -oac pcm -ovc copy download.mp4 -o output.mp4
+        mencoder -ss $cut_from_start -oac pcm -ovc copy download.mp4 -o output.mp4 -mc 0 -noskip
     else
-        mencoder -ss $cut_from_start -endpos $cut_from_end -oac pcm -ovc copy download.mp4 -o output.mp4
+        mencoder -ss $cut_from_start -endpos $cut_from_end -oac pcm -ovc copy download.mp4 -o output.mp4 -mc 0 -noskip
     fi
 fi
-# youtube-upload --title="$title" --privacy="$privacy" output.mp4
+youtube-upload --title="$title" --privacy="$privacy" output.mp4
 printf "\n\n"
-# rm -rf *
+rm -rf *
 echo "Sikeresen feltöltődött a videó"
